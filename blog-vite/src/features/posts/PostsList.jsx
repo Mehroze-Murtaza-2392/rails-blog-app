c13d394d011919a9ea92e290cd3d9ca32bc903bb
Mehroze <caterpillar2392@gmail.com>
2024-10-13 02:34:49 +0500

    Added new post form

--- blog-vite/src/features/posts/PostsList.jsx
+++ blog-vite/src/features/posts/PostsList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { API_URL } from "../../constants"
+import { Link } from 'react-router-dom';
 
 function PostsList () {
   const [posts, setPosts] = useState([]);
@@ -31,7 +32,9 @@ function PostsList () {
     <div>
       {posts.map((post) => (
         <div key={post.id} className='post-container'>
-          <h2>{post.title}</h2>
+          <Link to={`/posts/${post.id}`} className='post-title'>
+            <h2>{post.title}</h2>
+          </Link>
           <p>{post.body}</p>
         </div>
       ))}
