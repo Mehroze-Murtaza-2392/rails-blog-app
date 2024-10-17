d7313e47f73f95c93b23547684040da2ee15f7ca
Mehroze-Murtaza-2392 <159159598+Mehroze-Murtaza-2392@users.noreply.github.com>
2024-10-18 03:20:36 +0500

    Implemented delete post (#6)

--- blog-vite/src/features/posts/PostDetails.jsx
+++ blog-vite/src/features/posts/PostDetails.jsx
@@ -7,6 +7,7 @@ function PostDetails() {
     const [loading, setLoading] = useState(true)
     const [, setError] = useState(null)
     const { id } = useParams()
+    const navigate = useNavigate()
 
     useEffect(() => {
         const fetchCurrentPost = async () => {
@@ -26,13 +27,30 @@ function PostDetails() {
         fetchCurrentPost()
     }, [id])
 
-    if (loading) return <h2>Loading ...</h2>
+    const deletePost = async () => {
+        try {
+            const response = await fetch(`${API_URL}/${id}`, {
+                method: 'DELETE',
+            })
+            if (response.ok) {
+                navigate('/')
+            } else {
+                throw response
+            }
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    if (loading) return <h2>Loading ...</h2> // if (!post) return <h2>Loading...</h2>
 
     return (
         <>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
             <Link to='/'>Back to Posts</Link>
+            {" | "}
+            <button onClick={deletePost}>Delete</button>
         </>
     )
 }
